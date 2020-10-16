8995f49e3482389bc0dde5ee46d577680da2a04a
cbecker-student <72958907+cbecker-school@users.noreply.github.com>
2020-10-16 01:44:18 -0500

    Fixed 'hello world' from p. 166
    Fixed several issues with 'hello world' example

---
+++ C-Examples/hello_world.c
@@ -0,0 +1,15 @@
+/* Fixing syntactical errors in the 'hello world'
+* source code on p. 166
+*/
+
+#include <stdio.h>
+
+// again the function type and return type
+// need to be defined
+int main(void)
+{
+	printf("Hello, world!\n\n");
+
+	// again, this is required in main()
+	return (0);
+}
